2e191bdd47eea592529255f6e9cd1c171038321e
Carlos de Tena Bellmont <carlos.bellmont@gmail.com>
2021-03-29 23:07:27 +0200

    Updated to show a Snack if the API_MAPS_KEY isn't available

--- app/src/main/java/com/cbellmont/mapejemplo/MapsActivity.kt
+++ app/src/main/java/com/cbellmont/mapejemplo/MapsActivity.kt
@@ -3,28 +3,36 @@ package com.cbellmont.mapejemplo
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.cbellmont.mapejemplo.databinding.ActivityMapsBinding
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.snackbar.Snackbar
 
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
 
+    private lateinit var binding: ActivityMapsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_maps)
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
         // Sería el equivalente a poner
         // mapFragment.getMapAsync{
         // El código de onMapReady aquí
         //}
-
+        if (BuildConfig.MAPS_API_KEY.isEmpty()){
+            Snackbar.make(binding.root, "Debes poner tu MAPS_API_KEY en tu local.properties", Snackbar.LENGTH_INDEFINITE).show()
+        }
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
